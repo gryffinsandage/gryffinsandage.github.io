7b5d8f3f295b1b4fcafc0f8dab7e1d87da16efb2
gryffinsandage <gryfftime1223@gmail.com>
2026-03-19 17:57:42 +0000

    making game items

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -10,8 +10,23 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  const BOARD_WIDTH = $("#board").width();
+  const BOARD_HEIGHT = $("#board").height() ;
   // Game Item Objects
+  function GameItems(x, y, speedX, speedY, id){
+    return {
+      x: x,
+      y: y,
+      speedX: speedX,
+      speedY: speedY,
+      width: $(id).width(),
+      height:$(id).height(),
+      id: id,
+    }
+  }
+  var ball = GameItems(BOARD_WIDTH / 2, BOARD_HEIGHT / 2, (Math.random() > 0.5 ? -5 : 5), (Math.random() > 0.5 ? -1 : 1), "#ball")
+  var leftPaddle = GameItems(20, BOARD_HEIGHT / 2 - 125, 0, 0, "#leftPaddle"  );
+  var rightPaddle = GameItems(BOARD_WIDTH - 20, BOARD_HEIGHT / 2 - 125, 0, 0, "#rightPaddle"  );
 
 
   // one-time setup
@@ -27,21 +42,47 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    drawObject(ball);
+    drawObject(leftPaddle);
+    drawObject(rightPaddle);
+    moveObj(ball);
+    moveObj(leftPaddle);
+    moveObj(rightPaddle);
   }
   
   /* 
   Called in response to events.
   */
   function handleEvent(event) {
-
+    var keyCode = {
+      player2_up: 38,
+      player2_down: 40,
+      player1_up: 87,
+      player2_down: 83,
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+  function handleKeyDown(){
+    
+  }
+  
+  
+  
+  
+  
+  
+  function drawObject(gameItem){
+    $(gameItem.id).css("top", gameItem.y);
+    $(gameItem.id).css("left", gameItem.x);
 
+  }
+  function moveObj(gameItem){
+    gameItem.x += gameItem.speedX;
+    gameItem.y += gameItem.speedY;
+  }
   
   function endGame() {
     // stop the interval timer
