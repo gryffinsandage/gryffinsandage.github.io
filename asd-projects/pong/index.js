6da8bb1677825106296d219a8e143607e807d2aa
gryffinsandage <gryfftime1223@gmail.com>
2026-03-20 17:59:20 +0000

    PONG

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -12,6 +12,13 @@ function runProgram(){
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   const BOARD_WIDTH = $("#board").width();
   const BOARD_HEIGHT = $("#board").height() ;
+  const KEYCODE = {
+      UP: 38,
+      DOWN: 40,
+      W: 87,
+      S: 83,
+      ENTER: 13,
+    }
   // Game Item Objects
   function GameItems(x, y, speedX, speedY, id){
     return {
@@ -24,15 +31,15 @@ function runProgram(){
       id: id,
     }
   }
-  var ball = GameItems(BOARD_WIDTH / 2, BOARD_HEIGHT / 2, (Math.random() > 0.5 ? -5 : 5), (Math.random() > 0.5 ? -1 : 1), "#ball")
+  var ball = GameItems(BOARD_WIDTH / 2, BOARD_HEIGHT / 2, (Math.random() > 0.5 ? -10 : 10), (Math.random() > 0.5 ? -1 : 1), "#ball")
   var leftPaddle = GameItems(20, BOARD_HEIGHT / 2 - 125, 0, 0, "#leftPaddle"  );
-  var rightPaddle = GameItems(BOARD_WIDTH - 20, BOARD_HEIGHT / 2 - 125, 0, 0, "#rightPaddle"  );
+  var rightPaddle = GameItems(BOARD_WIDTH - 30, BOARD_HEIGHT / 2 - 125, 0, 0, "#rightPaddle"  );
 
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -48,32 +55,78 @@ function runProgram(){
     moveObj(ball);
     moveObj(leftPaddle);
     moveObj(rightPaddle);
+    collision(ball);
+    doCollideLeft();
+    doCollideRight();
+    paddleCollide(leftPaddle);
+    paddleCollide(rightPaddle);
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-    var keyCode = {
-      player2_up: 38,
-      player2_down: 40,
-      player1_up: 87,
-      player2_down: 83,
+  function handleKeyDown(event){
+    var keycode = event.which;
+    console.log(keycode);
+
+    if (keycode === KEYCODE.UP) {
+      rightPaddle.speedY = -10;
+    }else if (keycode === KEYCODE.DOWN) {
+      rightPaddle.speedY =  10;
+    }
+    if (keycode === KEYCODE.W) {
+      leftPaddle.speedY =  -10;
+    }else if (keycode === KEYCODE.S) {
+      leftPaddle.speedY =  10;
+    }  
+  }
+  function handleKeyUp(event){
+    var keycode = event.which
+    if (keycode === KEYCODE.UP) {
+      rightPaddle.speedY = 0;
+    }else if (keycode === KEYCODE.DOWN) {
+      rightPaddle.speedY = 0;
+    }
+    if (keycode === KEYCODE.W) {
+      leftPaddle.speedY =  0;
+    }else if (keycode === KEYCODE.S) {
+      leftPaddle.speedY =  0;
+    }
+
+  }
+  function collision(obj){
+    if(obj.x >= BOARD_WIDTH){
+      endGame();
+    }else if(obj.x <= 0){
+      endGame();
+    }else if(obj.y >= BOARD_HEIGHT - ball.height){
+      obj.speedY = -obj.speedY;
+    }else if(obj.y <= 0 + ball.height){
+      obj.speedY = -obj.speedY;
     }
   }
+  function paddleCollide(paddle){
+    if(paddle.y <= 0){
+      paddle.speedY = 0
+    }else if(paddle.y + paddle.height >= BOARD_HEIGHT){
+      paddle.speedY = 0
+    }
+  }
+  function doCollideLeft(){
+   if( ball.x <= leftPaddle.x - ball.width + leftPaddle.width && ball.y > leftPaddle.y && ball.y < leftPaddle.y + leftPaddle.height ){
+      ball.speedX = -ball.speedX
+   }
+}
+function doCollideRight(){
+  if(ball.x >= rightPaddle.x && ball.y > rightPaddle.y && ball.y < rightPaddle.y + rightPaddle.height){
+    ball.speedX = -ball.speedX
+  }
+}
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-  function handleKeyDown(){
-    
-  }
-  
-  
-  
-  
-  
-  
+
   function drawObject(gameItem){
     $(gameItem.id).css("top", gameItem.y);
     $(gameItem.id).css("left", gameItem.x);
@@ -91,5 +144,5 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
+
 }
