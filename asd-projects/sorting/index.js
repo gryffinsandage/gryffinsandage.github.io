d831e1977ff6d821c33c36a2b7036f3bf2634959
gryffinsandage <gryfftime1223@gmail.com>
2026-04-08 17:59:56 +0000

    sorting

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -9,12 +9,22 @@ The CSS ids you will work with are:
 
 */
 
+const { jsx } = require("react/jsx-runtime");
+
 ///////////////////////////////////////////////////////////////////////
 /////////////////////// YOUR WORK GOES BELOW HERE /////////////////////
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
-
+async function bubbleSort(array){
+    for(var i = 0; i < array.length - 1; i ++ ){
+        for(var j = array.length - 1; j < i + 1;   ){
+            if(array[j] < array[j - 1]){
+                swap()
+            }
+        }
+    }
+}
 
 // TODO 3: Implement quickSort
 
@@ -23,7 +33,12 @@ The CSS ids you will work with are:
 
 
 // TODO 1: Implement swap
-
+function swap(array, i, j){
+    var temp = array[i]
+    array[i] = array[j];
+    array[j] = temp;
+    return array;
+}
 
 ///////////////////////////////////////////////////////////////////////
 /////////////////////// YOUR WORK GOES ABOVE HERE /////////////////////
